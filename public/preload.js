c0d52dbea49a8bacb4d597f2468c29c5b612aca3
Ashley Matthews <ashleymatthews91@gmail.com>
2022-03-19 19:34:24 +0000

    Functionality to execute table files against VPX

--- public/preload.js
+++ public/preload.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const { spawn } = require('child_process');
 const { contextBridge } = require('electron');
+const settings = require('./settings.json');
 
 function getTableFiles(path) {
   const files = fs.readdirSync(path);
   return files.filter(f => f.endsWith('.vpx'));
 }
 
-contextBridge.exposeInMainWorld('files', { getTableFiles });
+function playTable(filePath) {
+  spawn(settings.executable, ['-minimized', '-play', filePath.replaceAll('/', '\\')]);
+}
+
+contextBridge.exposeInMainWorld('fileApi', { getTableFiles, playTable });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-const fileApi = window.files;
+const { fileApi } = window;
 
 const renderFileName = fileName => fileName.split('.vpx')[0];
 
@@ -12,28 +12,32 @@ const renderInput = (setFilePath, loadFileNames) =>
     <button onClick={loadFileNames}>Go</button>
   </div>;
 
-const renderTables = (fileNames) =>
+const renderTables = (fileNames, handleClick) =>
   <div id="tables">
     <h2>Available Tables</h2>
     <div id="tables-grid">
       {
-        fileNames.map(f => <div className="table" key={f}>{renderFileName(f)}</div>)
+        fileNames.map(f => <div className="table" onClick={() => handleClick(f)} key={f}>{renderFileName(f)}</div>)
       }
     </div>
   </div>;
 
 function App() {
 
-  const [filePath, setFilePath] = useState('');
+  const [filePath, setFilePath] = useState('F:/Pinball/Visual Pinball/Tables');
   const [fileNames, setFileNames] = useState([]);
 
   const loadFileNames = () => {
     setFileNames(fileApi.getTableFiles(filePath));
   }
 
+  const loadTable = (fileName) => {
+    fileApi.playTable(filePath + '/' + fileName);
+  }
+
   return (
     <div className="App">
-      {fileNames.length ? renderTables(fileNames) : renderInput(setFilePath, loadFileNames) }
+      {fileNames.length ? renderTables(fileNames, loadTable) : renderInput(setFilePath, loadFileNames) }
     </div>
   );
 }
